43fa2c9fa786b2a9fb7aed4846c6506cb296688e
LeiGGaRFeeD <ridefoot@gmail.com>
2026-03-29 16:28:57 +0400

    R5

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Side Apps-PlayableStudyCaseQuiz-1.0_vertical";
 const contentToCache = [
-    "Build/10VerticalFHD_R4.loader.js",
-    "Build/10VerticalFHD_R4.framework.js",
-    "Build/10VerticalFHD_R4.data",
-    "Build/10VerticalFHD_R4.wasm",
+    "Build/10VerticalFHD_R5.loader.js",
+    "Build/10VerticalFHD_R5.framework.js",
+    "Build/10VerticalFHD_R5.data",
+    "Build/10VerticalFHD_R5.wasm",
     "TemplateData/style.css"
 
 ];
